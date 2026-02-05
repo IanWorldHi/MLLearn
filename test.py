4dfab54a1397968876582c4ecc5df1fc3813359b
IanWorldHi <ianchensmiley@gmail.com>
2026-02-05 08:52:29 -0500

    test

---
+++ test.py
@@ -0,0 +1,24 @@
+import torch
+import numpy as np
+#Need to learn some numpy I guess
+# Faster becuase all in same part of memory, regular python lists are not
+
+
+#Tensors: like arrays/matrices to encode inputs/outputs
+#Can auto infer type
+data = [[1, 2], [1, 3]]
+torchdata = torch.tensor(data)  
+nparr = np.array(data)
+torcharr = torch.from_numpy(nparr)
+
+print(nparr)
+print(torcharr)
+
+
+
+
+
+
+
+
+
