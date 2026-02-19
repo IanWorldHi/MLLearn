088f95824c380c30df3dbf17555be80b18bc89d7
IanWorldHi <ianchensmiley@gmail.com>
2026-02-19 11:01:17 -0500

    photos n vids

---
+++ opencver/testLearn.py
@@ -0,0 +1,11 @@
+#(https://www.youtube.com/watch?v=oXlwWbU8l2o) for this they also do "pip install caer" which is a libray they built on top of opencv for easy of us, doubt ill need to use it tho
+
+import cv2 as cv
+
+
+
+
+
+
+
+
